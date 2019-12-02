68a74fdba377d88400571d21c8bfe93ed46182c5
Aparna9876 <58392587+Aparna9876@users.noreply.github.com>
2019-12-02 17:39:50 +0530

    Add files via upload

---
+++ ASCIIvalue.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+	char a;
+	cout<<"enter a character:";
+	cin>>a;
+	cout<<"ASCII value of"<<a<<"is"<<int(a);
+	getch();
+}
