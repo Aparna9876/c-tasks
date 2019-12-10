81670a2b92fbd05ea799635bf680ecfb636af9e0
Aparna9876 <58392587+Aparna9876@users.noreply.github.com>
2019-12-10 14:56:52 +0530

    Add files via upload

---
+++ task5.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int n,rev=0,rem=0;
+	printf("enter the number:\n");
+	scanf("%d",&n);
+	while(n>0)
+	{
+		rem=n%10;
+		rev=rev*10+rem;
+		n=n/10;
+	}
+	printf("the reversed number is :%d",rev);
+	getch();
+	
+}
