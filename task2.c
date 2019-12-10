dc8506c0678cea0471398e3e8de7a30c94558a10
Aparna9876 <58392587+Aparna9876@users.noreply.github.com>
2019-12-10 14:41:20 +0530

    Add files via upload

---
+++ task2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+float c,f;
+printf("enter temperature in celsius:");
+scanf("%f",&c);
+f=9*(c+32)/5;
+printf("temperature in fahrenheit is %f",f);
+getch();
+}
